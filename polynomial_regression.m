ea478032347d22006c5e8f3c86c9f210d7c98a7f
bolt25 <dharmik255@gmail.com>
2019-03-24 23:52:22 +0530

    Added new files

---
+++ polynomial_regression.m
@@ -0,0 +1,21 @@
+clear all;
+clc;
+
+order=input('Enter the order of the equation: ');
+number=input('Enter the number of datapoints: ');
+for i=1:number
+    x(i)=input('Enter X: ');
+    y(i)=input('Enter Y: ');
+end
+x=reshape(x,number,1);
+y=reshape(y,number,1);
+
+for i=1:(order+1)
+    for j=1:(order+1)
+        x_sum(i,j)= sum(x.^(i+j-2));
+        y_sum(i)=sum(x.^(i-1).*y);
+    end
+end
+y_sum=reshape(y_sum,length(y_sum),1);
+weights=(x_sum)\y_sum;
+disp(weights)
