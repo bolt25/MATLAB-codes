19208cbfef48702efe824f9c612c87fbeb62eb2a
bolt25 <dharmik255@gmail.com>
2019-03-27 23:16:50 +0530

    Added NFD and NBD

---
+++ NBD.m
@@ -0,0 +1,47 @@
+clc;
+clear all;
+disp('Enter equispaced values of X only');
+disp('---------------------------------');
+order=input('Enter the order of equation: ');
+number=order+1;
+disp('---------------------------------');
+for i=1:number
+    x(i)=input('Enter x: ');
+    y(i)=input('Enter y: ');
+end
+x=reshape(x,number,1);
+x=flipud(x);
+%flipud is a matlab function which flip a column vector - Up to Down
+%fliplr is a matlab function which flip a row vector - Left to Right
+y=reshape(y,number,1);
+y=flipud(y);
+
+%{
+ Here I used flipud to flip the column vectors of x and y so the rest of
+the NFD code can be used with minimum changes. Else entire matrix update
+loop would be reqd to be changed.
+%} 
+
+disp('---------------------------------');
+matrix=zeros(number,number+1);
+matrix(:,1:2)=[x,y];
+x_input=input('Enter the x value which you want to calculate: ');
+disp('---------------------------------');
+p=(x_input-x(1))/(x(1)-x(2));
+for j=3:number+1   % here column controlled loop is outer loop rather than row controlled loop
+    for i=1:number
+        if(i < length(matrix(:,1))-(j-3))  %every subsequent column has one element less then previous row rest all entries are zero for that row
+            matrix(i,j)= (-matrix(i+1,j-1)+matrix(i,j-1));   
+        end
+    end
+end
+coeff=matrix(1,2:end);
+p_matrix=ones(order+1,1);
+for i=2:length(x)
+    for j=1:(i-1)
+        p_matrix(i)=(p_matrix(i-1)*(p+(i-2)))/j; %j here accounts for the factorial term
+
+    end
+end
+answer=coeff*p_matrix;
+disp(answer);
--- NFD.m
+++ NFD.m
@@ -1,31 +1,35 @@
 clc;
 clear all;
+disp('Enter equispaced values of X only');
+disp('---------------------------------');
 order=input('Enter the order of equation: ');
-disp('------------------------------------');
 number=order+1;
+disp('---------------------------------');
 for i=1:number
     x(i)=input('Enter x: ');
     y(i)=input('Enter y: ');
 end
 x=reshape(x,number,1);
 y=reshape(y,number,1);
+disp('---------------------------------');
 matrix=zeros(number,number+1);
 matrix(:,1:2)=[x,y];
+x_input=input('Enter the x value which you want to calculate: ');
+disp('---------------------------------');
+p=(x_input-x(1))/(x(2)-x(1));
 for j=3:number+1   % here column controlled loop is outer loop rather than row controlled loop
     for i=1:number
         if(i < length(matrix(:,1))-(j-3))  %every subsequent column has one element less then previous row rest all entries are zero for that row
-            matrix(i,j)= (matrix(i+1,j-1)-matrix(i,j-1))/(matrix(i+j-2,1)-matrix(i,1));   
+            matrix(i,j)= (matrix(i+1,j-1)-matrix(i,j-1));   
         end
-      
     end
 end
 coeff=matrix(1,2:end);
-disp('---------------------------------');
-x_input=input('Enter the x value: ');
-disp('---------------------------------');
-x_matrix=ones(order+1,1);
+p_matrix=ones(order+1,1);
 for i=2:length(x)
-    x_matrix(i)=(x_matrix(i-1)*(x_input-x(i-1)));
+    for j=1:(i-1)
+        p_matrix(i)=(p_matrix(i-1)*(p-(i-2)))/j;   %j here accounts for the factorial term
+    end
 end
-answer=coeff*x_matrix;
+answer=coeff*p_matrix;
 disp(answer);
